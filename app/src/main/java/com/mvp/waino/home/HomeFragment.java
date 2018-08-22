5b16b2955bca2697497e229024ae15c1b187d06c
Waino <ya3924143@163.com>
2018-08-22 23:45:23 +0800

    提交代码一下

--- app/src/main/java/com/mvp/waino/home/HomeFragment.java
+++ app/src/main/java/com/mvp/waino/home/HomeFragment.java
@@ -1,21 +1,11 @@
 package com.mvp.waino.home;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+
 
 import com.mvp.waino.base.BaseFragment;
 import com.mvp.waino.mymvp.R;
-import com.trello.rxlifecycle2.LifecycleProvider;
-import com.trello.rxlifecycle2.LifecycleTransformer;
-import com.trello.rxlifecycle2.android.FragmentEvent;
 
-import io.reactivex.Observable;
-import io.reactivex.subjects.BehaviorSubject;
-import me.yokeyword.fragmentation.SupportFragment;
 
 /**
  * @author waino
@@ -25,7 +15,6 @@ import me.yokeyword.fragmentation.SupportFragment;
 public class HomeFragment extends BaseFragment {
 
     public static HomeFragment newInstance() {
-
         Bundle args = new Bundle();
 
         HomeFragment fragment = new HomeFragment();
